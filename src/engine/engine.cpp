194012ed157577887ed283de104b32bbcc7194ac
SneakyGerbil <68425553+SneakyGerbil@users.noreply.github.com>
2021-09-01 18:22:58 +0200

    Finished texture tutorial. Added gl math library.

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <stb_image.h>
+
 #include <shader.hpp>
 
 
@@ -175,6 +181,22 @@ int main(){
     //====================//
     //     Main Loop      //
     //====================//
+
+
+
+
+
+
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
+
+
+
+
+
 
     //Load and set shader
     Shader shader("./testShaders/vertexShader.glsl", "./testShaders/fragmentShader.glsl");
